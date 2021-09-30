6f6c257bb0a99a88d1b63b401baf6e1d40c5ca1a
Erdodif <erdodif@gmail.com>
2021-09-30 21:37:55 +0200

    Áttérés binding-ra, mert a kotlinx extension elavult

--- app/src/main/java/hu/petrik/thinkanumber/MainActivity.kt
+++ app/src/main/java/hu/petrik/thinkanumber/MainActivity.kt
@@ -2,10 +2,13 @@ package hu.petrik.thinkanumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import hu.petrik.thinkanumber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
